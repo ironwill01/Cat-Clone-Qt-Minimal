acade53ee7fbf343b8c39260541d7cfdec5cb4fe
ironwill <ironwillgit01@gmail.com>
2025-12-19 17:41:34 +0330

    Prototype my { cat } clone from linux

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,48 @@
-#include <iostream>
+#include <QCoreApplication>
+#include <QDir>
+#include <QFile>
+#include <memory>
+#include <QtLogging>
+#include <qlogging.h>
+#include <qobject.h>
 
-int main(void) {
-    std::cout << "hello world !" << std::endl;
+bool readFile(QFile & file) {
+ 
+    if(file.open(QIODevice::ReadOnly)) {
+        
+        while(!file.atEnd()) {
+            qInfo().noquote() << file.readLine();
+        }
+
+        return true;    
+    }
+
+    qFatal() << "File is not readable , should be a text or any readable format";
+    return false;
+}
+
+std::unique_ptr<QFile> getFile(const QString & path) {
+
+    if(!QFile::exists(path)) {
+        qWarning() << "Your file was not found !";
+        return nullptr;
+    }
+
+    std::unique_ptr<QFile> fileptr(new QFile(path));
+    return fileptr;
+}
+
+int main(int argc , char ** argv) {
+
+    QCoreApplication app(argc , argv);
+
+    if(app.arguments().count() < 2) {
+        qFatal() << "Cant start the app there is no path passed to program !";    
+    }
+
+    if(auto file = getFile(app.arguments().at(1))) {
+        if(!readFile(*file)) {
+            qFatal() << "Error : File you passed either was not readable or found exiting ...";
+        }        
+    }
 }
