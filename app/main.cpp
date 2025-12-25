7836797e03ed57e11ba8f695ba06a47d10df72b1
ironwill01 <ironwillgit01@gmail.com>
2025-12-25 20:03:47 +0330

    Fixed the issue that program crash because of no check for input

--- app/main.cpp
+++ app/main.cpp
@@ -3,8 +3,14 @@
 #include <QCommandLineParser>
 #include <Cat.hpp>
 #include <Flags.hpp>
+#include <qlogging.h>
 
 int main(int argc , char ** argv) {
+
+    if(argc < 2) {
+        qInfo() << "Usage : cat [options 1] [options 2] [options ...] <FilePath>";
+        exit(1);
+    }
 
     QCoreApplication app(argc , argv);
     std::unique_ptr<QCommandLineParser> parser = flagsInit(app);
